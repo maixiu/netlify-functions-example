a11f6c56be23d6271ee21d445fc2244a7662add0
Matthieu Tabuteau <m.tabuteau@gmail.com>
2020-03-06 00:25:46 +0100

    Update hello.js

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,10 +1,21 @@
 exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    headers: {
-      'content-type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-    },
-    body: "Hello, World"
-  });
+  if (event.httpMethod === 'OPTIONS') {
+    callback(null, {
+      statusCode: 204,
+      headers: {
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT',
+      },
+      body: {},
+    });
+  } else {
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: "Hello, World"
+    });
+  }
 };
