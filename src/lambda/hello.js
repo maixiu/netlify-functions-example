815d56d115eaa5d7f4355291b07cbde442444fe6
Matthieu Tabuteau <m.tabuteau@gmail.com>
2020-03-06 00:16:58 +0100

    Update hello.js

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,6 +1,10 @@
 exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
+    headers: {
+      'content-type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
     body: "Hello, World"
   });
 };
