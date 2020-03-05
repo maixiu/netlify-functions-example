2980177c4532252ab9c4203b755254660c518e8b
Matthieu Tabuteau <m.tabuteau@gmail.com>
2020-03-06 00:57:15 +0100

    Update hello.js

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,21 +1,9 @@
 exports.handler = function(event, context, callback) {
-  if (event.httpMethod === 'OPTIONS') {
-    callback(null, {
-      statusCode: 200,
-      headers: {
-        'Allow': 'OPTIONS,GET,POST,PUT,DELETE,PATCH',
-        'Access-Control-Allow-Methods': 'OPTIONS,GET,POST,PUT,DELETE,PATCH',
-        'Access-Control-Allow-Headers': 'content-type',
-        'Access-Control-Allow-Origin': 'http://localhost:4200'
-      }
-    });
-  } else {
-    callback(null, {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
-      body: "NOT Hello, World"
-    });
-  }
+  callback(null, {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: "NOT Hello, World"
+  });
 };
